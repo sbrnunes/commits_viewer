60221c586e04ec2fb2e13cac9a986740159067f3
Sergio Nunes <sbrnunes@gmail.com>
2019-09-10 22:05:18 +0100

    Refactoring tests using ScalaTest fixtures

--- src/test/scala/com/acme/commitviewer/cli/GitHubCLISpec.scala
+++ src/test/scala/com/acme/commitviewer/cli/GitHubCLISpec.scala
@@ -1,26 +1,32 @@
 package com.acme.commitviewer.cli
 
-import java.io.File
 import java.net.URL
 import java.time.Instant
 
 import com.acme.commitviewer.model.{Commit, Page}
 import com.acme.commitviewer.util.{Error, MD5}
 import org.scalamock.scalatest.MockFactory
-import org.scalatest.{FunSpecLike, Matchers}
+import org.scalatest.{Matchers, Outcome, fixture}
 
 import scala.reflect.io.Directory
 
-class GitHubCLISpec extends FunSpecLike with MockFactory with Matchers {
+class GitHubCLISpec extends fixture.FunSpecLike with MockFactory with Matchers {
+  val repo = new URL("http://some.repo.test/repo")
+  val cachedReposRoot = Directory("cached_repos")
+  val cachedRepo = cachedReposRoot / Directory(MD5.digest(repo.toString))
+  val limit = 10
+  val offset = 0
+
+  type FixtureParam = GitCLI
+
+  def withFixture(test: OneArgTest): Outcome = {
+    val git: GitCLI = mock[GitCLI]
+    test(git)
+  }
 
   describe("listCommits") {
-    val repo = new URL("http://some.repo.test/repo")
-    val cachedReposRoot = Directory("cached_repos")
-    val cachedRepo = cachedReposRoot / Directory(MD5.digest(repo.toString))
-    val limit = 10
-    val offset = 0
 
-    it("should use the Git CLI to setup the cached repo and fetch the list of commits") {
+    it("should use the Git CLI to setup the cached repo and fetch the list of commits") { implicit git: GitCLI =>
       val expected = List(
         Commit(
           "ref",
@@ -29,7 +35,6 @@ class GitHubCLISpec extends FunSpecLike with MockFactory with Matchers {
           date = Instant.now.toEpochMilli.toString,
           subject = "subject"))
 
-      implicit val git = mock[GitCLI]
       (git.clone _ ).expects(repo, cachedRepo).returning(Right(true))
       (git.pull _ ).expects(repo, cachedRepo).returning(Right(true))
       (git.log _ ).expects(repo, cachedRepo, limit, offset).returning(Right(expected))
@@ -38,10 +43,9 @@ class GitHubCLISpec extends FunSpecLike with MockFactory with Matchers {
       client.listCommits(repo, limit, offset) should equal(Right(Page(expected, nextOffset = 1)))
     }
 
-    it("should fail if the repo could not be cloned") {
+    it("should fail if the repo could not be cloned") { implicit git: GitCLI =>
       val expected = Left(Error("Some error"))
 
-      implicit val git: GitCLI = mock[GitCLI]
       (git.clone _ ).expects(repo, cachedRepo).returning(expected)
       (git.pull _ ).expects(*, *).never()
       (git.log _ ).expects(*, *, *, *).never()
@@ -50,10 +54,9 @@ class GitHubCLISpec extends FunSpecLike with MockFactory with Matchers {
       client.listCommits(repo, limit, offset) should equal(expected)
     }
 
-    it("should fail if the repo could not be updated") {
+    it("should fail if the repo could not be updated") { implicit git: GitCLI =>
       val expected = Left(Error("Some error"))
 
-      implicit val git: GitCLI = mock[GitCLI]
       (git.clone _ ).expects(repo, cachedRepo).returning(Right(true))
       (git.pull _ ).expects(repo, cachedRepo).returning(expected)
       (git.log _ ).expects(*, *, *, *).never()
@@ -62,10 +65,9 @@ class GitHubCLISpec extends FunSpecLike with MockFactory with Matchers {
       client.listCommits(repo, limit, offset) should equal(expected)
     }
 
-    it("should fail if the list of commits could not be retrieved") {
+    it("should fail if the list of commits could not be retrieved") { implicit git: GitCLI =>
       val expected = Left(Error("Some error"))
 
-      implicit val git: GitCLI = mock[GitCLI]
       (git.clone _ ).expects(repo, cachedRepo).returning(Right(true))
       (git.pull _ ).expects(repo, cachedRepo).returning(Right(true))
       (git.log _ ).expects(repo, cachedRepo, limit, offset).returning(expected)
